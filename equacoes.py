a7004904816e83ebe194209848d87ab760325dc5
giselegoulart <gisa_goulart9@hotmail.com>
2017-06-27 18:02:12 -0300

    prints metodo de verlet

--- equacoes.py
+++ equacoes.py
@@ -2,3 +2,33 @@ from __future__ import division
 import numpy as np
 import math
 import pylab as pl
+
+class Problema1:
+    def funcao1(self):
+        return 0
+    
+    def funcao2(self):
+        return 0
+        
+    def contono_X1(self):
+        return 0
+        
+    def contorno_Y1(self):
+        return 0
+    
+    def contono_X2(self):
+        return 0
+        
+    def contorno_Y2(self):
+        return 0
+        
+
+class Problema2:
+    def funcao(self):
+        return 0
+        
+    def contono_X(self):
+        return 0
+        
+    def contorno_Y(self):
+        return 0
--- metodos.py
+++ metodos.py
@@ -2,3 +2,17 @@ from __future__ import division
 import numpy as np
 import math
 import pylab as pl
+
+class MetodosDiscretos:
+    def EulerExplicito(self):
+        return 0
+    
+    def RangeKutta2(self):
+        return 0
+        
+    def RangeKutta4(self):
+        return 0
+        
+class MetodosContorno:
+    def Verlet(self):
+        return 0
